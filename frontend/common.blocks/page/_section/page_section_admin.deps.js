8029aac057d6e100116e255bfe84a9298037f12f
alexbaum <alexbaum@yandex-team.ru>
2015-06-24 19:12:48 +0300

    Fix view deps

--- frontend/common.blocks/page/_section/page_section_admin.deps.js
+++ frontend/common.blocks/page/_section/page_section_admin.deps.js
@@ -6,6 +6,10 @@
         {
             block: 'radio-group',
             mods: {theme: 'islands', size: 'm', type: 'button'}
+        },
+        {
+            block: 'menu',
+            mods: {theme: 'islands', size: 'm', section: 'admin'}
         }
     ]
 });
